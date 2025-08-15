76c80028471df240d43190f76447d2c5797d74a9
Rishav <rishavanand1997@gmail.com>
2025-08-15 23:54:20 +0530

    updating contact

--- src/App.jsx
+++ src/App.jsx
@@ -78,8 +78,8 @@ function App() {
               </p>
               <div className="contact-details">
                 <p><strong>Address:</strong> SRC-24-B, Shipra Rivera, Indirapuram, Ghaziabad, UP 201014</p>
-                <p><strong>Email:</strong> yevacurepharmaceutical123@gmail.com</p>
-                <p><strong>Phone:</strong> +91-8047785182</p>
+                <p><strong>Email:</strong> yevacurepharmaceuticals@gmail.com</p>
+                <p><strong>Phone:</strong> +91-9871285873</p>
               </div>
             </div>
 
